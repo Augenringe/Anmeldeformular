860d92648c6f9beff738c577278d61c631ec7d87
Nico Rausch <nicorausch110406@gmail.com>
2025-06-04 13:37:45 +0200

    Add files via upload

---
+++ Anmeldeformular/main.js
@@ -0,0 +1,17 @@
+const loginForm = document.getElementById("login");
+const registerForm = document.getElementById("register");
+const btn = document.getElementById("btn");
+
+function register() {
+  document.getElementById("btn").style.left = "50%"
+  loginForm.style.left = "-400px";
+  registerForm.style.left = "50px";
+  btn.style.left = "110px";
+}
+
+function login() {
+  document.getElementById("btn").style.left = "0";
+  loginForm.style.left = "50px";
+  registerForm.style.left = "450px";
+  btn.style.left = "0";
+}
